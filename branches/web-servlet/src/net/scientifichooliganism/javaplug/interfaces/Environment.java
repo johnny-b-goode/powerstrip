b5251c9690bcb8ef517cf0b446039c18111624fe
kyle-lamse <kyle-lamse@268a2f48-c709-4d62-8028-0511accead18>
2016-08-02 20:16:44 +0000

    Fleshed out more interfaces

--- branches/web-servlet/src/net/scientifichooliganism/javaplug/interfaces/Environment.java
+++ branches/web-servlet/src/net/scientifichooliganism/javaplug/interfaces/Environment.java
@@ -1,5 +1,8 @@
 package net.scientifichooliganism.javaplug.interfaces;
 
 public interface Environment extends ValueObject{
-
+    String getName();
+    void setName(String in);
+    String getDescription();
+    void setDescription(String in);
 }
--- branches/web-servlet/src/net/scientifichooliganism/javaplug/interfaces/Release.java
+++ branches/web-servlet/src/net/scientifichooliganism/javaplug/interfaces/Release.java
@@ -1,5 +1,14 @@
 package net.scientifichooliganism.javaplug.interfaces;
 
 public interface Release extends ValueObject {
-
+    String getApplication();
+    void setApplication(String in);
+    String getName();
+    void setName(String in);
+    String getDescription();
+    void setDescription(String in);
+    Date getDueDate();
+    void setDueDate(String in);
+    Date getReleaseDate();
+    void setReleaseDate(Date in);
 }
