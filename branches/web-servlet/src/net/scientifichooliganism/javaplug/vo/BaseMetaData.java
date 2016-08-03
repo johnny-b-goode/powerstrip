dfe1b1fece83dd6a316b3fbdbe3cbddae6164247
tylerhatwigcgi <tylerhatwigcgi@268a2f48-c709-4d62-8028-0511accead18>
2016-08-03 16:45:24 +0000

    Merging changes from trunk into branch.

---
+++ branches/web-servlet/src/net/scientifichooliganism/javaplug/vo/BaseMetaData.java
@@ -0,0 +1,160 @@
+package net.scientifichooliganism.javaplug.vo;
+
+public class MetaData extends ValueObject {
+	private String object;
+	private String objectID;
+	private int sequence;
+	private String key;
+	private String value;
+
+	public MetaData () {
+		super();
+		object = null;
+		objectID = null;
+		sequence = -1;
+		key = null;
+		value = null;
+	}
+
+	private void validateObjectSet () throws IllegalStateException {
+		String message = "MetaData.validateObjectSet() an attempt was made to put the object into an invalid state";
+
+		if ((object == null) || (object.length() <= 0)) {
+			throw new IllegalStateException(message);
+		}
+
+		if ((objectID == null) || (objectID.length() <= 0)) {
+			throw new IllegalStateException(message);
+		}
+	}
+
+	private void validateObject () throws IllegalStateException {
+		String message = "MetaData.validateObject() an attempt was made to put the object into an invalid state";
+
+		if (objectID != null) {
+			if ((object == null) || (object.length() <= 0)) {
+				throw new IllegalStateException(message);
+			}
+		}
+	}
+
+	private void validateKeyValue () throws IllegalStateException {
+		String message = "MetaData.validateKeyValue() an attempt was made to put the object into an invalid state";
+
+		if (value != null) {
+			if ((key == null) || (key.length() <= 0)) {
+				throw new IllegalStateException(message);
+			}
+		}
+	}
+
+	//an idea I was toying with
+	/*
+	public void attach (ValueObject vo) throws IllegalArgumentException, RuntimeException {
+		if (vo == null) {
+			throw new IllegalArgumentException("attach(ValueObject) was called with a null ValueObject");
+		}
+
+		if (((getKey() == null) || (getKey().length() <= 0)) && ((getValue() == null) || (getValue().length() <= 0))) {
+			throw new RuntimeException("attach(ValueObject) was called before the key or value was set for the MetaData object.");
+		}
+
+		if ((getObject() == null) || (getObject().length() <= 0)) {
+			setObject(vo.getClass().getName());
+		}
+
+		if ((getObjectID() == null) || (getObjectID().length() <= 0)) {
+			setObjectID(vo.getID());
+		}
+
+		vo.addMetaData(this);
+	}
+	*/
+
+	public String toString () {
+		String ret = super.toString();
+		ret = ret + "object: " + String.valueOf(object) + "\n";
+		ret = ret + "objectID: " + String.valueOf(objectID) + "\n";
+		ret = ret + "sequence: " + String.valueOf(sequence) + "\n";
+		ret = ret + "key: " + String.valueOf(key) + "\n";
+		ret = ret + "value: " + String.valueOf(value) + "\n";
+		return ret;
+	}
+
+	public String getObject () {
+		return object;
+	}
+
+	public void setObject (String in) throws IllegalArgumentException {
+		if (in == null) {
+			throw new IllegalArgumentException("setObject(String) was called with a null string");
+		}
+
+		if (in.length() <= 0) {
+			throw new IllegalArgumentException("setObject(String) was called with an empty string");
+		}
+
+		object = in;
+	}
+
+	public String getObjectID () {
+		return objectID;
+	}
+
+	public void setObjectID (String in) throws IllegalArgumentException {
+		if (in == null) {
+			throw new IllegalArgumentException("setObjectID(String) was called with a null string");
+		}
+
+		if (in.length() <= 0) {
+			throw new IllegalArgumentException("setObjectID(String) was called with an empty string");
+		}
+
+		objectID = in;
+		validateObject();
+	}
+
+	public int getSequence () {
+		return sequence;
+	}
+
+	public void setSequence (int in) throws IllegalArgumentException {
+		if (in < 0) {
+			throw new IllegalArgumentException("setSequence(int) was called with a value less than zero");
+		}
+
+		sequence = in;
+	}
+
+	public String getKey () {
+		return key;
+	}
+
+	public void setKey (String in) throws IllegalArgumentException {
+		if (in == null) {
+			throw new IllegalArgumentException("setKey(String) was called with a null string");
+		}
+
+		if (in.length() <= 0) {
+			throw new IllegalArgumentException("setKey(String) was called with an empty string");
+		}
+
+		validateObjectSet();
+		key = in;
+		validateKeyValue();
+	}
+
+	public String getValue () {
+		return value;
+	}
+
+	public void setValue (String in) throws IllegalArgumentException {
+		if (in == null) {
+			throw new IllegalArgumentException("setValue(String) was called with a null string");
+		}
+
+		validateObjectSet();
+		value = in;
+		validateKeyValue();
+	}
+}
--- branches/web-servlet/src/net/scientifichooliganism/javaplug/vo/BaseValueObject.java
+++ branches/web-servlet/src/net/scientifichooliganism/javaplug/vo/BaseValueObject.java
@@ -2,13 +2,18 @@ package net.scientifichooliganism.javaplug.vo;
 
 import net.scientifichooliganism.javaplug.interfaces.ValueObject;
 
+import java.util.Collection;
+import java.util.Vector;
+
 public class BaseValueObject implements ValueObject {
 	private String id;
 	private String label;
+	private Collection<MetaData> metadata;
 
 	public BaseValueObject() {
 		id = null;
 		label = null;
+		metadata = new Vector<MetaData>();
 	}
 
 	public String getID () {
@@ -40,9 +45,35 @@ public class BaseValueObject implements ValueObject {
 		label = in;
 	}
 
+	public Collection<MetaData> getMetaData () {
+		return metadata;
+	}
+
+	public void addMetaData (MetaData md) throws IllegalArgumentException {
+		removeMetaData(md);
+		metadata.add(md);
+	}
+
+	public void removeMetaData (MetaData md) throws IllegalArgumentException {
+		if (md == null) {
+			throw new IllegalArgumentException("removeMetaData(MetaData) was called with a null object");
+		}
+
+		if (metadata.contains(md)) {
+			metadata.remove(md);
+		}
+	}
+
 	public String toString () {
 		String ret = "label: " + String.valueOf(label) + "\n";
 		ret = ret + "id: " + String.valueOf(id) + "\n";
+		ret = ret + "metadata:\n";
+
+		//TODO: clean up the (lack of) formatting this will cause
+		for (MetaData md : metadata) {
+			ret = ret + "	" + md.toString();
+		}
+
 		return ret;
 	}
 
